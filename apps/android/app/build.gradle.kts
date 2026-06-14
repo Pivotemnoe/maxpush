007d851b4172a652e3376f0504de08c669c325d5
Pivotemnoe <Pivotemnoe@users.noreply.github.com>
2026-06-14 19:40:52 +0300

    Bump Android release version

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "ru.temichev.maxpush"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "DEFAULT_API_BASE_URL", "\"$defaultApiBaseUrl\"")
         buildConfigField("String", "ALLOWED_API_HOST", "\"$allowedApiHost\"")
